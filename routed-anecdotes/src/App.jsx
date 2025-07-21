1aeb0ea710b6b8224a28be86732934159a30524d
dev-chris21 <chrispinmaamba01@gmail.com>
2025-07-21 15:34:55 +0200

    exercises for part7 a

---
+++ routed-anecdotes/src/App.jsx
@@ -0,0 +1,108 @@
+import { useState } from 'react'
+import {
+  BrowserRouter as Router,
+  Routes, Route, Link
+} from 'react-router-dom'
+import About from './components/About'
+import AnecdoteList from './components/AnecdoteList'
+import CreateNew from './components/CreateNew'
+import Anecdote from './components/Anecdote'
+
+const Menu = () => {
+  const padding = {
+    paddingRight: 5
+  }
+  return (
+    <div>
+      <a href='/anecdotes' style={padding}>anecdotes</a>
+      <a href='/create new' style={padding}>create new</a>
+      <a href='/about' style={padding}>about</a>
+    </div>
+  )
+}
+
+const Footer = () => (
+  <div>
+    Anecdote app for <a href='https://fullstackopen.com/'>Full Stack Open</a>.
+
+    See <a href='https://github.com/fullstack-hy2020/routed-anecdotes/blob/master/src/App.js'>https://github.com/fullstack-hy2020/routed-anecdotes/blob/master/src/App.js</a> for the source code.
+  </div>
+)
+
+
+
+const App = () => {
+  const [anecdotes, setAnecdotes] = useState([
+    {
+      content: 'If it hurts, do it more often',
+      author: 'Jez Humble',
+      info: 'https://martinfowler.com/bliki/FrequencyReducesDifficulty.html',
+      votes: 0,
+      id: 1
+    },
+    {
+      content: 'Premature optimization is the root of all evil',
+      author: 'Donald Knuth',
+      info: 'http://wiki.c2.com/?PrematureOptimization',
+      votes: 0,
+      id: 2
+    }
+  ])
+
+  const [notification, setNotification] = useState('')
+
+  const addNew = (anecdote) => {
+    anecdote.id = Math.round(Math.random() * 10000)
+    setAnecdotes(anecdotes.concat(anecdote))
+    setNotification(`a new anecdote ${anecdote.content} created!`)
+    setTimeout(() => {
+      setNotification('')
+    }, 5000);
+  }
+
+  const anecdoteById = (id) =>
+    anecdotes.find(a => a.id === id)
+
+  const vote = (id) => {
+    const anecdote = anecdoteById(id)
+
+    const voted = {
+      ...anecdote,
+      votes: anecdote.votes + 1
+    }
+
+    setAnecdotes(anecdotes.map(a => a.id === id ? voted : a))
+  }
+
+  const padding = {
+    paddingRight: 5
+  }
+
+  return (
+     <Router>
+      <h1>Software Anecdotes</h1>
+      <div>
+        <Link style={padding} to="/">anecdotes</Link>
+        <Link style={padding} to="/create new">create new</Link>
+        <Link style={padding} to="/about">about</Link>
+      </div>
+
+      <div>
+        {notification && <div className='notification'>{notification}</div>}
+      </div>
+
+      <Routes>
+        <Route path="/anecdotes/:id" element={<Anecdote anecdotes={anecdotes} />} />
+        <Route path="/" element={<AnecdoteList anecdotes={anecdotes}/>} />
+        <Route path="/create new" element={<CreateNew addNew={addNew}/>} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+
+      <div>
+        <Footer/>
+      </div>
+    </Router>
+  )
+}
+
+export default App
